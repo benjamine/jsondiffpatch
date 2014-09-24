2a0a2509b18b0e8a41c5c312feff812a795cb3b5
Benjamin Eidelman <beneidel@gmail.com>
2014-09-24 02:33:09 -0300

    0.1.12

--- public/build/jsondiffpatch-full.js
+++ public/build/jsondiffpatch-full.js
@@ -3244,7 +3244,7 @@ exports.reverse = function() {
 
 if (process.browser) {
 	exports.homepage = 'https://github.com/benjamine/jsondiffpatch';
-	exports.version = '0.1.11';
+	exports.version = '0.1.12';
 } else {
 	var packageInfoModuleName = '../package.json';
 	var packageInfo = require(packageInfoModuleName);
