18602a37cf90e40cd3c8ff4807deb0102d55a537
Benjamin Eidelman <beneidel@gmail.com>
2015-09-04 20:39:56 -0300

    exposing base formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,10 @@
 var environment = require('../environment');
 
+exports.base = require('./base');
 exports.html = require('./html');
 exports.annotated = require('./annotated');
 
 if (!environment.isBrowser) {
-	var consoleModuleName = './console';
-	exports.console = require(consoleModuleName);
+  var consoleModuleName = './console';
+  exports.console = require(consoleModuleName);
 }
