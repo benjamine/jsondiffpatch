e3dfc645a32bde11c17d547827a586fe6fb5e376
Benjamin Eidelman <beneidel@gmail.com>
2015-09-04 21:10:45 -0300

    version bump

--- build/jsondiffpatch.js
+++ build/jsondiffpatch.js
@@ -43,7 +43,7 @@ exports.reverse = function() {
 
 if (environment.isBrowser) {
 	exports.homepage = 'https://github.com/benjamine/jsondiffpatch';
-	exports.version = '0.1.34';
+	exports.version = '0.1.36';
 } else {
 	var packageInfoModuleName = '../package.json';
 	var packageInfo = require(packageInfoModuleName);
