d4c21a25814d3d5ce0f7df56b2d1a173439e5c61
Alexis Cote <acote@coveo.com>
2018-04-13 17:11:21 -0400

    Fix main exports

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-export * from './core';
+// Expose the core functions
+export {
+  DiffPatcher,
+  create,
+  diff,
+  patch,
+  unpatch,
+  reverse,
+  clone,
+} from './core';
+
+// Export all formatters
 export * as formatters from './formatters/index';
+
+// Export other components
 export * as console from './formatters/console';
