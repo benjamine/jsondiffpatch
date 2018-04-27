d4c79182a8e4c684fa43431e8a5a7273265f8aff
Arnar Þór Sveinsson <arnar.th92@gmail.com>
2018-04-27 11:27:37 -0700

    Try adding clone to patch

--- src/contexts/patch.js
+++ src/contexts/patch.js
@@ -1,4 +1,5 @@
 import Context from './context';
+import defaultClone from '../clone';
 
 class PatchContext extends Context {
   constructor(left, delta) {
@@ -6,6 +7,22 @@ class PatchContext extends Context {
     this.left = left;
     this.delta = delta;
     this.pipe = 'patch';
+  }
+
+  setResult(result) {
+    if (this.options.cloneDiffValues && typeof result === 'object') {
+      const clone =
+        typeof this.options.cloneDiffValues === 'function'
+          ? this.options.cloneDiffValues
+          : defaultClone;
+      if (typeof result[0] === 'object') {
+        result[0] = clone(result[0]);
+      }
+      if (typeof result[1] === 'object') {
+        result[1] = clone(result[1]);
+      }
+    }
+    return Context.prototype.setResult.apply(this, arguments);
   }
 }
 
