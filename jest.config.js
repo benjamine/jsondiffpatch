64af500c2b75cf5ca04e8fa55203832a52f5a3f5
Nathan Bierema <nbierema@gmail.com>
2023-08-20 21:54:23 -0400

    Add coverage threshold limits (#341)

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+const config = {
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+};
+
+module.exports = config;
