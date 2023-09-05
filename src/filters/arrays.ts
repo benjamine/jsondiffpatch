789aa63742da254a94970593c30c527046e669a3
Nathan Bierema <nbierema@gmail.com>
2023-09-04 22:08:27 -0400

    Add bad type cast

--- src/filters/arrays.ts
+++ src/filters/arrays.ts
@@ -512,7 +512,8 @@ export const collectChildrenReverseFilter: Filter<ReverseContext> = (
       name = reverseArrayDeltaIndex(arrayDelta, child.childName!, child.result);
     }
     if (delta[name] !== child.result) {
-      delta[name] = child.result;
+      // TODO Is this right and, if so, how should it be expressed?
+      delta[name] = child.result as DeletedDelta | MovedDelta;
     }
   }
   context.setResult(delta).exit();
