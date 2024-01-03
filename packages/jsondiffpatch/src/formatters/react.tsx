7493843a06a4fe276ae7db273de816122b32bd16
John Dunn <johnwdunn20@gmail.com>
2024-01-03 11:58:09 -0800

    updated prettier

--- packages/jsondiffpatch/src/formatters/react.tsx
+++ packages/jsondiffpatch/src/formatters/react.tsx
@@ -7,25 +7,32 @@ import './styles/html.css';
 type ReactFormatterType = {
   oldJson: unknown;
   newJson?: unknown;
-  initallyShowDiff? : boolean;
-}
-// write unit tests for this component
+  initallyShowDiff?: boolean;
+};
 
-const ReactFormatterComponent: React.FC<ReactFormatterType> = ({ oldJson, newJson, initallyShowDiff }) => {
-
-  const [showDiffOnly, setShowDiffOnly] = useState(initallyShowDiff)
+const ReactFormatterComponent: React.FC<ReactFormatterType> = ({
+  oldJson,
+  newJson,
+  initallyShowDiff,
+}) => {
+  const [showDiffOnly, setShowDiffOnly] = useState(initallyShowDiff);
 
   const delta: Delta = diff(oldJson, newJson);
   if (!delta) {
     return <></>;
   }
   const htmlDiff = new HTMLFormatter().format(delta, oldJson);
-  const createMarkupHtml = () => ({ __html: htmlDiff } || '') as { __html: TrustedHTML };
-  
+  const createMarkupHtml = () =>
+    (({ __html: htmlDiff }) || '') as { __html: TrustedHTML };
+
   return (
-    <div className={`json-diff-container ${showDiffOnly ? 'jsondiffpatch-unchanged-hidden' : ''}`}>
-        <div dangerouslySetInnerHTML={createMarkupHtml()}></div>
-  </div>
+    <div
+      className={`json-diff-container ${
+        showDiffOnly ? 'jsondiffpatch-unchanged-hidden' : ''
+      }`}
+    >
+      <div dangerouslySetInnerHTML={createMarkupHtml()}></div>
+    </div>
   );
 };
 
